32bf7b440e649ec884cd96f590b3de5c339629e9
David Moller <davidmollera@hotmail.com>
2023-10-24 10:08:59 -0500

    created and imported the home page to the app file and created a home css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./CSS/App.css";
+import Home from "./Pages/Home";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={}/>
+        <Route path="/" element={ <Home /> }/>
       </Routes>
     </Router>
   );
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../CSS/Home.css'
+
+const Home = () => {
+    return (
+        <section id="home"></section>
+    );
+}
+
+export default Home;
