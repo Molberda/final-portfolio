ca9042ac207e51a3e5f8bcb9356fdc6207356025
David Moller <davidmollera@hotmail.com>
2023-10-25 09:19:59 -0500

    installed the mui icons and added basic html to the nav component

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import '../CSS/Nav.css'
+import React from "react";
+import "../CSS/Nav.css";
+import { Menu } from "@mui/icons-material";
 
 const Nav = () => {
-    return (
-        <nav>
-            
-        </nav>
-    );
-}
+  return (
+    <nav>
+      <figure className="nav__logo--wrapper">
+        <img src="" alt="" className="nav__logo" />
+      </figure>
+      <Menu />
+    </nav>
+  );
+};
 
 export default Nav;
