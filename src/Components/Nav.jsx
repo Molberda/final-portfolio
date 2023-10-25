936c3e7b8327ac74fdc2c4c57108dde1df4f2181
David Moller <davidmollera@hotmail.com>
2023-10-25 09:53:02 -0500

    added an icon button to wrap the menu icon and used UseNavigate hook to navigate between pages

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import "../CSS/Nav.css";
 import { Menu } from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
+import { IconButton } from "@mui/material";
 
 const Nav = () => {
+
+  const navTo = useNavigate();
+
   return (
     <nav>
-      <figure className="nav__logo--wrapper">
+      <figure className="nav__logo--wrapper" onClick={() => navTo('/')}>
+        test
         <img src="" alt="" className="nav__logo" />
       </figure>
-      <Menu />
+      <IconButton onClick={() => navTo("/menu")}>
+        <Menu />
+      </IconButton>
     </nav>
   );
 };
