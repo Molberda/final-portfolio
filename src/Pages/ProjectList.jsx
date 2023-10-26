1e7e6f430c1718732a97cc107f1961579f1dfbcf
David Moller <davidmollera@hotmail.com>
2023-10-26 12:31:43 -0500

    created projectList page and css file

---
+++ src/Pages/ProjectList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../CSS/ProjectList.css'
+
+const ProjectList = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default ProjectList;
