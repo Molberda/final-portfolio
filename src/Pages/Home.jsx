b0de42b58dc418ae1a83908ae93b00bd3171300d
David Moller <davidmollera@hotmail.com>
2023-10-27 15:25:31 -0500

    added the nav function to the home page and implemented it on the buttons

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,34 +3,43 @@ import "../CSS/Home.css";
 import { Button } from "@mui/material";
 import { ArrowOutward } from "@mui/icons-material";
 import { motion as m } from "framer-motion";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navTo = useNavigate();
 
   return (
-      <m.section
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1, transition: {duration: 1.5}}}
-        exit={{ opacity: 0, transitionDuration: .5 }}
-        id="home"
-        className="top__section"
-      >
-        <div className="container home__container">
-          <div className="row home__row">
-            <div className="home__title">
-              <h1>Hello! My name is</h1>
-              <span className="highlight">
-                <p>David Möller</p>
-                <div className="hover-effect"></div>
-              </span>
-              <h1>I'm a frontend developer</h1>
-            </div>
-            <div className="home__btns">
-              <Button startIcon={<ArrowOutward />}>My Projects</Button>
-              <Button startIcon={<ArrowOutward />}>About Me</Button>
-            </div>
+    <m.section
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1, transition: { duration: 1.5 } }}
+      exit={{ opacity: 0, transitionDuration: 0.5 }}
+      id="home"
+      className="top__section"
+    >
+      <div className="container home__container">
+        <div className="row home__row">
+          <div className="home__title">
+            <h1>Hello! My name is</h1>
+            <span className="highlight">
+              <p>David Möller</p>
+              <div className="hover-effect"></div>
+            </span>
+            <h1>I'm a frontend developer</h1>
+          </div>
+          <div className="home__btns">
+            <Button
+              onClick={() => navTo("/projects")}
+              startIcon={<ArrowOutward />}
+            >
+              My Projects
+            </Button>
+            <Button onClick={() => navTo("about")} startIcon={<ArrowOutward />}>
+              About Me
+            </Button>
           </div>
         </div>
-      </m.section>
+      </div>
+    </m.section>
   );
 };
 
