72ec9ce1e7a7ca515330e3852c2dfc7ba83d3749
David Moller <davidmollera@hotmail.com>
2023-11-01 12:56:58 -0500

    use state hook to define the language of the app and display the home link in the logo accordingly

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,23 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "../CSS/Nav.css";
 import { Menu } from "@mui/icons-material";
 import { useNavigate } from "react-router-dom";
 import { Button, IconButton } from "@mui/material";
 
 const Nav = () => {
+  const [language, setLanguage] = useState("english");
+
   const navTo = useNavigate();
+
+  const translate = (value, lang) => {
+    setLanguage(lang);
+    navTo(value);
+  };
 
   return (
     <nav>
-      <figure className="nav__logo--wrapper" onClick={() => navTo("/")}>
-        <h1>DM</h1>
-        <img src="" alt="" className="nav__logo" />
-      </figure>
+      {language === "english" ? (
+        <figure className="nav__logo--wrapper" onClick={() => navTo("/")}>
+          <h1>DM</h1>
+          <img src="" alt="" className="nav__logo" />
+        </figure>
+      ) : (
+        <figure className="nav__logo--wrapper" onClick={() => navTo("/es")}>
+          <h1>DM</h1>
+          <img src="" alt="" className="nav__logo" />
+        </figure>
+      )}
       <div className="nav__right">
-        <Button onClick={() => navTo("/")}>
+        <Button onClick={() => translate("/", "english")}>
           <p>🇺🇸</p>
         </Button>
-        <Button onClick={() => navTo("/es")}>
+        <Button onClick={() => translate("/es", "spanish")}>
           <p>🇪🇸</p>
         </Button>
         <IconButton onClick={() => navTo("/menu")}>
@@ -25,12 +39,8 @@ const Nav = () => {
         </IconButton>
       </div>
       <div className="nav__right--desktop">
-        <Button onClick={() => navTo("/")}>
-          <p>🇺🇸</p>
-        </Button>
-        <Button onClick={() => navTo("/es")}>
-          <p>🇪🇸</p>
-        </Button>
+        <Button onClick={() => navTo("/")}>Eng</Button>
+        <Button onClick={() => navTo("/es")}>Esp</Button>
       </div>
     </nav>
   );
