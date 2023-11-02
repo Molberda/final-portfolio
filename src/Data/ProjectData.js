cce4fc97c9b5de35d69c245490a90710ff13dea4
David Moller <davidmollera@hotmail.com>
2023-11-02 10:34:18 -0500

    added the project data for project #1

--- src/Data/ProjectData.js
+++ src/Data/ProjectData.js
@@ -10,6 +10,16 @@ export const Projects = [
     link: "https://barco-react-app.vercel.app",
   },
   {
+    id: 1,
+    name: "Delizioso",
+    nameEsp: "Delizioso",
+    description:
+      "This project was made for an entrepreneur chef looking to showcase her products and create a way for new customers to contact her. It's an original design and it uses HTML, CSS, JavaScript and React. It's a very simple website yet it acomplishes the goal that the client desired.",
+    descriptionEsp: "prueba de descripcion en espanol para delizioso",
+    img: "",
+    link: "https://jb-online-catalog.vercel.app",
+  },
+  {
     id: 2,
     name: "Barco",
     nameEsp: "Barco",
@@ -21,16 +31,6 @@ export const Projects = [
   },
   {
     id: 3,
-    name: "Barco",
-    nameEsp: "Barco",
-    description:
-      "This is a website for a Colombian bar hence why its in spanish, it was designed and developed by me. It uses HTML, CSS, JavaScript and React. Some of the elements present on this project are the use of loading states for a better user experience, the items on the menu are renderized using a mock up API so its more dynamic and easier to add or remove new products, it also has working filters so the customers can filter out the type of product they want. Lastly it is fully optimized for desktops and smartphones.",
-    descriptionEsp: "prueba de descripcion en espanol para barco",
-    img: "",
-    link: "https://barco-react-app.vercel.app",
-  },
-  {
-    id: 4,
     name: "Barco",
     nameEsp: "Barco",
     description:
