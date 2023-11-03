e7f786d7cd6cfb305ca47d307be6d29e5135f54c
David Moller <davidmollera@hotmail.com>
2023-11-03 10:04:12 -0500

    added the barco img to the data file

--- src/Data/ProjectData.js
+++ src/Data/ProjectData.js
@@ -1,3 +1,5 @@
+import barco from '../Assets/Barco-landing.png'
+
 export const Projects = [
   {
     id: 0,
@@ -6,7 +8,7 @@ export const Projects = [
     description:
       "This is a website for a Colombian bar hence why its in spanish, it was designed and developed by me. It uses HTML, CSS, JavaScript and React. Some of the elements present on this project are the use of loading states for a better user experience, the items on the menu are renderized using a mock up API so its more dynamic and easier to add or remove new products, it also has working filters so the customers can filter out the type of product they want. Lastly it is fully optimized for desktops and smartphones.",
     descriptionEsp: "prueba de descripcion en espanol para barco",
-    img: "",
+    img: barco ,
     link: "https://barco-react-app.vercel.app",
   },
   {
