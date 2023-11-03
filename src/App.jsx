f93ff1bf33c059129e81229037980028792ba803
David Moller <davidmollera@hotmail.com>
2023-11-03 09:35:43 -0500

    started styling the project focus page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router } from "react-router-dom";
-import "./CSS/App.css";
 import Nav from "./Components/Nav";
 import AnimatedRoutes from "./Components/AnimatedRoutes";
+import "./CSS/App.css";
 
 function App() {
   return (
--- src/Pages/ProjectFocus.jsx
+++ src/Pages/ProjectFocus.jsx
@@ -1,34 +1,31 @@
 import React from "react";
-import "../CSS/ProjectFocus.css";
 import { useNavigate, useParams } from "react-router-dom";
 import { ArrowOutward } from "@mui/icons-material";
 import { motion as m } from "framer-motion";
 import { Button } from "@mui/material";
 import { Projects } from "../Data/ProjectData";
-
+import "../CSS/ProjectFocus.css";
 
 const ProjectFocus = () => {
   const { id } = useParams();
-  const project = Projects.find((i) => i.id === +id)
+  const project = Projects.find((i) => i.id === +id);
   const navTo = useNavigate();
   return (
     <m.section
       initial={{ opacity: 0 }}
       animate={{ opacity: 1, transition: { duration: 2.5, ease: "easeInOut" } }}
       exit={{ opacity: 0, transitionDuration: 1 }}
-      id="home"
+      id="projectFocus"
       className="top__section"
     >
-      <div className="container home__container">
-        <div className="row home__row">
-          <div className="home__title">
-            <h1>this is project focus section</h1>
+      <div className="container projectFocus__container">
+        <div className="row projectFocus__row">
+          <div className="projectFocus__title">
             <span className="highlight">
-              <p>{project.name}</p>
-              <div className="hover-effect"></div>
+              <h1>{project.name}</h1>
             </span>
           </div>
-          <div className="home__btns">
+          <div className="projectFocus__btns">
             <Button
               onClick={() => navTo("/projects")}
               startIcon={<ArrowOutward />}
