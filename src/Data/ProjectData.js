4ab9fd5d3ff4eaa0db4e6c20364c1c6fdbc1e4a1
David Moller <davidmollera@hotmail.com>
2023-11-07 03:48:07 -0500

    imported the metrocar img to the asset folder and used it on the project data file

--- src/Data/ProjectData.js
+++ src/Data/ProjectData.js
@@ -1,6 +1,7 @@
 import barco from '../Assets/Barco-landing.png'
 import delizioso from '../Assets/Delizioso-landing.png'
 import google from '../Assets/Google-landing.png'
+import metrocar from '../Assets/metrocar.png'
 
 export const Projects = [
   {
@@ -40,7 +41,7 @@ export const Projects = [
     description:
       "This is a project developed for a car rental startup. It uses HMLT, CSS, JavaScript, React and MUI for the user to have the best experience. It has different elements such as maps integration and image slideshows that were developed from scratch, it also has a translate functionality that allows the user to experience the app equally in english and spanish as the company targets a international audience. It also has features such as a form that collects the data from the user and sends it directly to the company's email or more subtle features such as highlighting a nav link when its active. Finally its fully functional in all screens (Small phones, Regular Phones, Tablets, Laptops & Desktops).",
     descriptionEsp: "prueba de descripcion en espanol para MetroCar",
-    img: "",
+    img: metrocar,
     link: "https://metrocar.com.co",
   },
 ];
