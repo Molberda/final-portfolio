cd68caecb967cdec83afa6362307133c36bc5737
David Moller <davidmollera@hotmail.com>
2023-11-07 04:00:54 -0500

    added an arrow back icon to the project focus page and all of its css

--- src/Pages/ProjectFocus.jsx
+++ src/Pages/ProjectFocus.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { ArrowOutward } from "@mui/icons-material";
+import { ArrowBackIosNew, ArrowOutward } from "@mui/icons-material";
 import { motion as m } from "framer-motion";
-import { Button } from "@mui/material";
+import { Button, IconButton } from "@mui/material";
 import { Projects } from "../Data/ProjectData";
 import "../CSS/ProjectFocus.css";
 
@@ -20,6 +20,9 @@ const ProjectFocus = () => {
       <div className="container projectFocus__container">
         <div className="row projectFocus__row">
           <div className="projectFocus__title">
+            <IconButton>
+              <ArrowBackIosNew/>
+            </IconButton>
             <span className="highlight">
               <h1>{project.name}</h1>
             </span>
